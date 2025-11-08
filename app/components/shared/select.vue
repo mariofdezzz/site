fea919a76c163bf91c3e4b400e6fdf2cf71448b9
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2025-11-08 18:35:04 +0000

    feat: better select dark mode support

--- app/components/shared/select.vue
+++ app/components/shared/select.vue
@@ -71,7 +71,6 @@ option {
 	border-radius: 5px;
 	/*  resets from UA  */
 	min-inline-size: 0;
-	box-shadow: 0px 12.8px 28.8px rgba(0, 0, 0, 0.13),
-		0px 0px 9.2px rgba(0, 0, 0, 0.11);
+	box-shadow: var(--shadow-elevated);
 }
 </style>
